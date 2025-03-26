9257470fc493f680aae2bbba4c7307fb93b620f5
VictorMurilloC <vmurillocarrasco@byupathway.edu>
2025-03-25 23:59:46 -0500

    Good

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer Jr";
+        job1._company = "Microsoft";
+        job1._startYear = 2014;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer Senior";
+        job2._company = "Apple";
+        job2._startYear = 2018;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Victor Murillo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
